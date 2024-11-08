c5ccfb1b6cc6b505fc83717573e45457443cf03d
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-11-08 12:55:10 +0530

    Time: 225 ms (55.49%) | Memory: 70.2 MB (43.14%) - LeetSync

--- 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
+++ 3637-count-number-of-balanced-permutations/count-number-of-balanced-permutations.cpp
@@ -1,57 +1,59 @@
 class Solution {
 public:
-    #define ll long long
-    const int MOD = 1e9+7;
-
-    ll power(ll x, ll y, ll p) { 
-        ll res = 1;
-        x = x % p;
-        if (x == 0) return 0; 
-
-        while (y > 0) { 
-            if (y & 1) 
-                res = (res * x) % p; 
-            y = y >> 1;
-            x = (x * x) % p; 
-        } 
-        return res; 
+#define ll long long
+int mod=1e9+7;
+    ll  power(ll x, ll y, ll p) 
+{ 
+    ll res = 1;     // Initialize result 
+ 
+    x = x % p; // Update x if it is more than or 
+                // equal to p
+  
+    if (x == 0) return 0; // In case x is divisible by p;
+ 
+    while (y > 0) 
+    { 
+        // If y is odd, multiply x with result 
+        if (y & 1) 
+            res = (res*x) % p; 
+ 
+        // y must be even now 
+        y = y>>1; // y = y/2 
+        x = (x*x) % p; 
     } 
-
+    return res; 
+} 
     int countBalancedPermutations(string num) {
-        vector<int> fr(10);
-        int sum = 0;
-        for (auto c : num) {
-            fr[c - '0']++;
-            sum += (c - '0');
+        vector<int>mp(10);int sum=0;
+        for(int i=0;i<num.size();i++){
+            mp[num[i]-'0']++;
+            sum+=num[i]-'0';
         }
-        
-        if (sum % 2 != 0) return 0;
-        int n = num.size();
-        vector<int> fact(n + 1, 1), ifact(n + 1, 1);
-        
-        for (int i = 1; i <= n; i++) {
-            fact[i] = (1LL * i * fact[i - 1]) % MOD;
-            ifact[i] = power(fact[i], MOD - 2, MOD);  // Use modular inverse correctly
-        }
+        if(sum&1)return 0;
+        int n=num.size();
+        vector<int>fact(n+1,1),ifact(n+1,1);
 
-        int permutationsCount = (1LL * fact[n / 2] * fact[(n + 1) / 2]) % MOD;
-        vector<vector<vector<int>>> dp(10, vector<vector<int>>(sum / 2 + 1, vector<int>(n / 2 + 1, -1)));
+        for(int i=1;i<=n;i++)
+       { fact[i]=(1LL*i*fact[i-1])%mod;
+        ifact[i]=power(fact[i],mod-2,mod);}
 
-        std::function<int(int, int, int)> f = [&](int number, int taken, int currentSum) -> int {
-            if (number == 10) {
-                return (currentSum == sum / 2 && taken == n / 2 ? permutationsCount : 0);
+       long long k=(1LL*fact[n/2]*fact[(n+1)/2])%mod;
+        vector<vector<vector<int>>>dp(10,vector<vector<int>>(sum/2+1,vector<int>(n/2+1,-1)));
+        auto f=[&](int d,int s,int l,auto &&self)-> int{
+              if(d==10){
+                if(s==sum/2&&l==n/2){
+                    return k;
+                }
+                return 0;
+              }
+              if(s>sum/2||l>n/2)return 0;
+            if(dp[d][s][l]!=-1)return dp[d][s][l];
+            long long way=0;
+            for(int i=0;i<=min(mp[d],n/2-l);i++){
+              way=(way+(1LL*(1LL*ifact[i]*ifact[mp[d]-i])%mod*self(d+1,s+i*d,l+i,self))%mod)%mod;
             }
-            if (currentSum > sum / 2 || taken > n / 2) return 0;
-            if (dp[number][currentSum][taken] != -1) return dp[number][currentSum][taken];
-
-            long long ways = 0;
-            for (int i = 0; i <= std::min(fr[number], n / 2 - taken); i++) {
-                ways = (ways + (1LL * ifact[i] * ifact[fr[number] - i] % MOD * f(number + 1, taken + i, currentSum + i * number)) % MOD) % MOD;
-            }
-            
-            return dp[number][currentSum][taken] = ways;
+            return dp[d][s][l]=way;
         };
-
-        return f(0, 0, 0);
+        return f(0,0,0,f);
     }
 };
