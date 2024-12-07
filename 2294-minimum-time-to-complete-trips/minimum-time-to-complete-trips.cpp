bf533c8562c9bca50bc7f48d55823d3c6fcf0670
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-07 09:54:47 +0530

    Time: 98 ms (11.71%) | Memory: 98.3 MB (20.47%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minimumTime(vector<int>& nums, int m) {
+         int n=nums.size();
+        
+        auto help=[&](long long mid){
+            long long sum=0;
+           for(int i=0;i<n;i++){
+             sum+=mid/nums[i];
+             if(sum>=1e15)return true;
+           }
+           return sum>=m;
+        };
+        long long ans;
+        long long low=1,high=1e15;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(help(mid)){
+                ans=mid;
+               high=mid-1;
+            }
+            else
+             low=mid+1;
+        }
+        return ans;
+    }
+};
