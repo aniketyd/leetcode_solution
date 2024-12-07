d510ae6c636fa420fa75eb351aa3c93d66db5648
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-07 09:56:44 +0530

    Time: 60 ms (60.92%) | Memory: 98.4 MB (14.16%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -7,12 +7,12 @@ public:
             long long sum=0;
            for(int i=0;i<n;i++){
              sum+=mid/nums[i];
-             if(sum>=1e15)return true;
+             if(sum>=m)return true;
            }
            return sum>=m;
         };
         long long ans;
-        long long low=1,high=1e15;
+        long long low=1,high=1LL*m*(*max_element(nums.begin(),nums.end()));
         while(low<=high){
             long long mid=low+(high-low)/2;
             if(help(mid)){
