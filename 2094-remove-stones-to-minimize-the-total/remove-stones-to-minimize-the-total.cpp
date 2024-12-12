3caa38e8b2833c48bbced4b691a8c46cb787be60
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-12 11:40:35 +0530

    Time: 171 ms (98.76%) | Memory: 102.9 MB (66.29%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int minStoneSum(vector<int>& g, int k) {
-         int n=g.size();
-     long long sum=0;
-     priority_queue<int>pq;
-     for(int i=0;i<n;i++){
-        pq.push(g[i]);
-     }
-     for(int i=0;i<k;i++){
-        int t=pq.top();
-        pq.pop();
-        pq.push(t/2+t%2);
-     }
-     while(!pq.empty()){
-        sum+=pq.top();
-        pq.pop();
-     }
-     return sum;
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq(piles.begin(), piles.end());
+        int total = accumulate(piles.begin(), piles.end(), 0);
+        for (int i=0; i<k; ++i) {
+            int cur = pq.top();
+            if (cur==1) break;
+            pq.pop();
+            int remove = cur/2;
+            cur -= remove;
+            total -= remove;
+            pq.push(cur);
+        }
+        return total;
     }
 };
