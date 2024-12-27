f5dd4c4d53b97054c0190ab2a75f565d589b3dc7
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-27 22:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (50.41%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -33,6 +33,29 @@ int dp[102][102];
         int n2=s2.size();
         int n=s3.size();
         if(n1+n2!=n)return false;
-        return f(s1,s2,s3,n1-1,n2-1,n-1);
+        vector<vector<bool>>dp(n1+1,vector<bool>(n2+1,false));
+        dp[0][0]=true;
+        for(int i=0;i<n1;i++){
+            if(s1[i]==s3[i])
+            dp[i+1][0]=dp[i][0];
+            else 
+            break;
+        }
+        for(int i=0;i<n2;i++){
+            if(s2[i]==s3[i])
+            dp[0][i+1]=dp[0][i];
+            else
+            break;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                int k=i+j-1;
+                if(i>=1&&s1[i-1]==s3[k])
+                dp[i][j]=dp[i][j]|dp[i-1][j];
+                if(j>=1&&s2[j-1]==s3[k])
+                dp[i][j]=dp[i][j]|dp[i][j-1];
+            }
+        }
+        return dp[n1][n2];
     }
 };
