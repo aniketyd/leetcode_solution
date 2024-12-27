b7bf8df2b09197828892f788bc4fb8d2a2b59200
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-27 22:06:30 +0530

    Time: 5 ms (45.26%) | Memory: 8.4 MB (85.08%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -33,29 +33,28 @@ int dp[102][102];
         int n2=s2.size();
         int n=s3.size();
         if(n1+n2!=n)return false;
-        vector<vector<bool>>dp(n1+1,vector<bool>(n2+1,false));
-        dp[0][0]=true;
-        for(int i=0;i<n1;i++){
-            if(s1[i]==s3[i])
-            dp[i+1][0]=dp[i][0];
-            else 
-            break;
-        }
+        vector<bool>dp(n2+1);
+        dp[0]=true;
+       
         for(int i=0;i<n2;i++){
             if(s2[i]==s3[i])
-            dp[0][i+1]=dp[0][i];
+            dp[i+1]=dp[i];
             else
             break;
         }
         for(int i=1;i<=n1;i++){
+            vector<bool>crr(n2+1);
+            if(s1[i-1]==s3[i-1])
+            crr[0]=dp[0];
             for(int j=1;j<=n2;j++){
                 int k=i+j-1;
                 if(i>=1&&s1[i-1]==s3[k])
-                dp[i][j]=dp[i][j]|dp[i-1][j];
+                crr[j]=crr[j]|dp[j];
                 if(j>=1&&s2[j-1]==s3[k])
-                dp[i][j]=dp[i][j]|dp[i][j-1];
+                crr[j]=crr[j]|crr[j-1];
             }
+            dp=crr;
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
