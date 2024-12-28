615f4c0f8abe70efc0bb5a66ac6b9ee7dbcd35cb
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-28 10:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (94.85%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -1,41 +1,84 @@
 class Solution {
 public:
-//dp[i][j]=maximum sum of j subarray from 0 to i 
-//dp[i][j]=max(dp[i-1][j],pf[i]-pf[i-k]+dp[i-k][j-1]); 
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(4,0));
-        
-        vector<int>pf(n+1);
-        for(int i=0;i<n;i++)
-        pf[i+1]+=pf[i]+nums[i];
-        for(int j=1;j<=3;j++){
-            for(int i=j*k;i<=n;i++){
-              dp[i][j]=max(dp[i-1][j],pf[i]-pf[i-k]+dp[i-k][j-1]);
-              
+        // Variables to track the best indices for one, two, and three subarray
+        // configurations
+        int bestSingleStart = 0;
+        vector<int> bestDoubleStart = {0, k};
+        vector<int> bestTripleStart = {0, k, k * 2};
 
+        // Compute the initial sums for the first three subarrays
+        int currentWindowSumSingle = 0;
+        for (int i = 0; i < k; i++) {
+            currentWindowSumSingle += nums[i];
+        }
+
+        int currentWindowSumDouble = 0;
+        for (int i = k; i < k * 2; i++) {
+            currentWindowSumDouble += nums[i];
+        }
+
+        int currentWindowSumTriple = 0;
+        for (int i = k * 2; i < k * 3; i++) {
+            currentWindowSumTriple += nums[i];
+        }
+
+        // Track the best sums found so far
+        int bestSingleSum = currentWindowSumSingle;
+        int bestDoubleSum = currentWindowSumSingle + currentWindowSumDouble;
+        int bestTripleSum = currentWindowSumSingle + currentWindowSumDouble +
+                            currentWindowSumTriple;
+
+        // Sliding window pointers for the subarrays
+        int singleStartIndex = 1;
+        int doubleStartIndex = k + 1;
+        int tripleStartIndex = k * 2 + 1;
+
+        // Slide the windows across the array
+        while (tripleStartIndex <= nums.size() - k) {
+            // Update the sums using the sliding window technique
+            currentWindowSumSingle = currentWindowSumSingle -
+                                     nums[singleStartIndex - 1] +
+                                     nums[singleStartIndex + k - 1];
+            currentWindowSumDouble = currentWindowSumDouble -
+                                     nums[doubleStartIndex - 1] +
+                                     nums[doubleStartIndex + k - 1];
+            currentWindowSumTriple = currentWindowSumTriple -
+                                     nums[tripleStartIndex - 1] +
+                                     nums[tripleStartIndex + k - 1];
+
+            // Update the best single subarray start index if a better sum is
+            // found
+            if (currentWindowSumSingle > bestSingleSum) {
+                bestSingleStart = singleStartIndex;
+                bestSingleSum = currentWindowSumSingle;
             }
-        }
-        vector<int>ans;
-        int i;
-        for( i=n-1;i>=1;i--){
-             if(dp[i][3]!=dp[i+1][3])
-            { ans.push_back(i-k+1);i-=k;break;}
-        }
-        for(;i>=1;i--){
-            if(dp[i][2]!=dp[i+1][2])
-            {
-                ans.push_back(i-k+1);i-=k;break;
+
+            // Update the best double subarray start indices if a better sum is
+            // found
+            if (currentWindowSumDouble + bestSingleSum > bestDoubleSum) {
+                bestDoubleStart[0] = bestSingleStart;
+                bestDoubleStart[1] = doubleStartIndex;
+                bestDoubleSum = currentWindowSumDouble + bestSingleSum;
             }
-        }
-         for(;i>=0;i--){
-            if(dp[i][1]!=dp[i+1][1])
-            {
-                ans.push_back(i-k+1);i--;break;
+
+            // Update the best triple subarray start indices if a better sum is
+            // found
+            if (currentWindowSumTriple + bestDoubleSum > bestTripleSum) {
+                bestTripleStart[0] = bestDoubleStart[0];
+                bestTripleStart[1] = bestDoubleStart[1];
+                bestTripleStart[2] = tripleStartIndex;
+                bestTripleSum = currentWindowSumTriple + bestDoubleSum;
             }
+
+            // Move the sliding windows forward
+            singleStartIndex += 1;
+            doubleStartIndex += 1;
+            tripleStartIndex += 1;
         }
-        
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        // Return the starting indices of the three subarrays with the maximum
+        // sum
+        return bestTripleStart;
     }
 };
