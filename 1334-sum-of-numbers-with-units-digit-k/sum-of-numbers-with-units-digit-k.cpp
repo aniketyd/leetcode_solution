ab25f80cddba880201f3fbfd0bfbf434b25fae2c
aniketyd <125073425+aniketyd@users.noreply.github.com>
2024-12-31 00:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (46.67%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-  int dp[3003];
-  int fun(int n, int k){
-    if(n==0)return 0;
-    if(n<0)return 1e9;
-    if(dp[n]!=-1)return dp[n];
-    int ans=1e9;
-    for(int i=k;i<=n;i+=10)
-     ans=min(ans,1+fun(n-i,k));
-     return dp[n]=ans;
-  }
     int minimumNumbers(int num, int k) {
-        if(num==0)
-        return 0;
-        if(k==0){
-            if(num%10==0)
-            return 1;
-            return -1;
+        if(num==0) return 0;
+        for(int n=1;n<=10 && n*k<=num;n++){
+            if((num-n*k)%10==0) return n;
         }
-        memset(dp,-1,sizeof(dp));
-        int ans=fun(num,k);
-        if(ans>=1e8)return -1;
-        return ans;
+        return -1;
     }
 };
