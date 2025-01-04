bcd69ecfc62e7e7f615af980c81f35bd39045526
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-01-04 10:02:50 +0530

    Time: 32 ms (96.32%) | Memory: 13.8 MB (96.63%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int fun(string &s,int i, int j){
+    int fun(int i){
         int c=0;
-        if(i>j)return 0;
-
-        vector<int>mp(27,0);
-        for(i;i<=j;i++){
-          mp[s[i]-'a']++;
+        while(i>0){
+            if(i%2==1)c++;
+            i/=2;
         }
-        for(int k=0;k<26;k++)
-        if(mp[k]>0)c++;
         return c;
     }
     int countPalindromicSubsequence(string s) {
@@ -19,8 +15,11 @@ public:
            while(k<n&&s[k]!=ch)k++;
            int j=n-1;
            while(j>=0&&s[j]!=ch)j--;
-          
-           ans+=fun(s,k+1,j-1);
+           int msk=0;
+          for(int t=k+1;t<=j-1;t++){
+              msk|=(1<<s[t]-'a');
+          }
+           ans+=fun(msk);
         }
         return ans;
     }
