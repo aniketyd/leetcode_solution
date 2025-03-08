75dbb2cb1d083689a4bc01451efe2e054f1302aa
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-08 10:47:11 +0530

    Time: 17 ms (20.71%) | Memory: 19 MB (59.99%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return NULL;
+        Node *start=root;
+        while(start){
+            Node *current=start;
+            while(current){
+                if(current->left){
+                    current->left->next=current->right;
+                }
+                if(current->right&&current->next){
+                    current->right->next=current->next->left;
+                }
+                current=current->next;
+            }
+            start=start->left;
+        }
+        return root;
+    }
+};
