364eb7f6d4214a89d404f0a266632ee313fba6dc
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-21 17:45:41 +0530

    Time: 1017 ms (59.42%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,6 +1,7 @@
-# Write your MySQL query statement below
-select round(avg(case when a.player_id is not NULL then 1 else 0 end),2) as fraction
-from
-(select player_id as pid,min(event_date) as ed from activity group by player_id )as t1 left join
-activity as a 
-on DATE_ADD(ed, INTERVAL 1 DAY)=a.event_date and pid=a.player_id; 
+select round(count(*)/(select count(distinct player_id) from activity),2) as fraction
+from activity
+where (player_id,event_date) in 
+(select player_id,DATE_ADD(MIN(event_date),INTERVAL 1 DAY)
+from activity 
+group by player_id
+)
