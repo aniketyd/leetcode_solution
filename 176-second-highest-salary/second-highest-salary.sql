9989ac74d3d3c09fc61c80164de80f366b149853
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-22 14:38:04 +0530

    Time: 479 ms (60.35%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,6 @@
-SELECT COALESCE((
-    SELECT DISTINCT e1.salary 
-    FROM Employee e1
-    JOIN Employee e2 
-    ON e1.salary < e2.salary
-    GROUP BY e1.salary
-    HAVING COUNT(DISTINCT e2.salary) = 1
-    LIMIT 1  -- Ensures only one result is returned
-), NULL) AS SecondHighestSalary;
+# Write your MySQL query statement below
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
