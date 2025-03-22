f2f63339a1541286122000fb6bf67553f8e99ef8
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-22 10:21:21 +0530

    Time: 734 ms (38.66%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,6 +1,6 @@
 SELECT 
     CASE 
-        WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM Seat) THEN id + 1
+        WHEN id % 2 = 1 AND id != (SELECT count(*) FROM Seat) THEN id + 1
         WHEN id % 2 = 0 THEN id - 1
         ELSE id
     END AS id,
