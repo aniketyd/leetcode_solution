bfdb65d7a9d3dc7c475da259e520bafcbb776ed5
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-22 10:18:51 +0530

    Time: 352 ms (95.06%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,17 +1,20 @@
-# Write your MySQL query statement below
-
-select id,student from
-(select s1.id ,
-case
-when s2.student is NULL then s1.student
-else s2.student
-end as student
-from (select * from seat where id%2=1) as s1 left join seat
-as s2 on s1.id+1=s2.id
-union all
-select s1.id ,
-s2.student as student
-from (select * from seat where id%2=0) as s1 left join seat
-as s2 on s1.id-1=s2.id) as t
-order by id
-;
+SELECT id, student
+FROM (
+    SELECT 
+        s1.id,
+        CASE
+            WHEN s2.student IS NULL THEN s1.student
+            ELSE s2.student
+        END AS student
+    FROM (SELECT * FROM seat WHERE id % 2 = 1) AS s1
+    LEFT JOIN seat AS s2 ON s1.id + 1 = s2.id
+    
+    UNION ALL
+    
+    SELECT 
+        s1.id,
+        s2.student
+    FROM (SELECT * FROM seat WHERE id % 2 = 0) AS s1
+    LEFT JOIN seat AS s2 ON s1.id - 1 = s2.id
+) AS swapped
+ORDER BY id;
