117899119f2822a2ca8ae3f25265e743c41bae61
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-22 14:18:45 +0530

    Time: 329 ms (99.61%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select patient_id,patient_name,conditions
 from patients
-where Locate(' DIAB1',conditions)>0 or left(conditions,5)='DIAB1';
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
