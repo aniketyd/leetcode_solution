e700e33cbc981bacf2123ac1de6c46792089496a
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-22 10:20:13 +0530

    Time: 976 ms (12.40%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,20 +1,9 @@
-SELECT id, student
-FROM (
-    SELECT 
-        s1.id,
-        CASE
-            WHEN s2.student IS NULL THEN s1.student
-            ELSE s2.student
-        END AS student
-    FROM (SELECT * FROM seat WHERE id % 2 = 1) AS s1
-    LEFT JOIN seat AS s2 ON s1.id + 1 = s2.id
-    
-    UNION ALL
-    
-    SELECT 
-        s1.id,
-        s2.student
-    FROM (SELECT * FROM seat WHERE id % 2 = 0) AS s1
-    LEFT JOIN seat AS s2 ON s1.id - 1 = s2.id
-) AS swapped
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
 ORDER BY id;
