ee633cab75b39161d7195fd3b2c5c0bc9781f380
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-03-26 09:53:58 +0530

    Time: 216 ms (98.39%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,6 @@
-SELECT a.machine_id,
-ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
-FROM Activity a
-JOIN Activity b 
-ON a.machine_id = b.machine_id
-AND a.process_id = b.process_id
-WHERE a.activity_type = "start"
-AND b.activity_type = "end"
-GROUP BY a.machine_id;
+select machine_id,
+round(sum(case
+when activity_type='end' then timestamp
+else -1*timestamp end)/count(distinct process_id),3) as processing_time
+from activity
+group by machine_id;
