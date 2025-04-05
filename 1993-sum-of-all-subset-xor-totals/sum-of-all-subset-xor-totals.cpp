17420d8dca2ab3df7df29c2e85a555cca82b8687
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-04-05 10:25:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (90.00%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(1025, -1)); // memo table
-        return XORSum(nums, 0, 0, dp);
-    }
-
-private:
-    int XORSum(vector<int>& nums, int index, int currentXOR, vector<vector<int>>& dp) {
-        if (index == nums.size()) return currentXOR;
-
-        if (dp[index][currentXOR] != -1) return dp[index][currentXOR];
-
-        int withElement = XORSum(nums, index + 1, currentXOR ^ nums[index], dp);
-        int withoutElement = XORSum(nums, index + 1, currentXOR, dp);
-
-        return dp[index][currentXOR] = withElement + withoutElement;
+        int result = 0;
+        for (int num : nums) {
+            result |= num;
+        }
+        return result << (nums.size() - 1);
     }
 };
