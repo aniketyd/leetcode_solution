df5cd6cb0e83c3a01b79585646a928d8955fe2d9
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-04-20 21:52:55 +0530

    Time: 16 ms (60.84%) | Memory: 38.5 MB (28.54%) - LeetSync

--- 1905-design-authentication-manager/design-authentication-manager.cpp
+++ 1905-design-authentication-manager/design-authentication-manager.cpp
@@ -1,60 +1,40 @@
-class AuthenticationManager {
+class AuthenticationManager 
+{
+    int t;
+    int uniques{};
+    unordered_map<string, int> st;
+    queue<pair<string, int>> q;
+    
 public:
-    
-    // universal lifetime 
-    int lifeTime;
-    
-    // record the token id and lifetime with unique id
-    unordered_map<string,pair<int,int>>records;
-    
-    // unique incremental id of tokens
-    int tokenCount;
-    
-    // store the lifetime of tokens MSB = lifetime, LSB = unique id
-    set<long long>tokenLife;
-    
-    AuthenticationManager(int timeToLive) {
-        
-        lifeTime = timeToLive;
-        tokenCount = 1;
+    AuthenticationManager(int t) : t{t} 
+    {   
     }
     
-    // Add new token uniqueId and lifetime in hash table and Set
-    void generate(string tokenId, int currentTime) {
-        
-        long long alive = currentTime + lifeTime;
-        long long id = tokenCount;
-        records[tokenId] = {currentTime + lifeTime,tokenCount};
-        tokenLife.insert(alive<<32 | id);
-        tokenCount++;
+    void generate(string s, int ct) 
+    {
+        st[s] = ct+t-1;
+        ++uniques;
+        q.push({s, ct+t-1});
     }
     
-    void renew(string tokenId, int currentTime) {
-        
-        //Check if tokenId exist in hash table and lifetime > current time
-        // Remove the previous lifetime from Set and update Set
-        // and hash table with updated lifetime
-        if(records.count(tokenId) && records[tokenId].first>currentTime)
+    void drop(int ct)
+    {
+        for( ; !empty(q) and q.front().second<ct; q.pop())
+            if(st[q.front().first]==q.front().second and  st[q.front().first]<ct) --uniques; 
+    }
+    
+    void renew(string s, int ct) 
+    {
+        if(st[s]>=ct)
         {
-            long long id = records[tokenId].second;
-            long long alive = records[tokenId].first;
-            tokenLife.erase(tokenLife.find(alive<<32 | id));
-            alive = currentTime + lifeTime;
-            records[tokenId].first = currentTime + lifeTime;
-            tokenLife.insert(alive<<32 | id);
+            st[s] = ct+t-1;
+            q.push({s, ct+t-1});
         }
     }
     
-    int countUnexpiredTokens(int currentTime) {
-        
-        // add tokenCount with current time as current time can be a expire time as well
-        long long time = currentTime;        
-        time = time<<32 | (long long)tokenCount;
-        
-        // search distance from begin to upper bound of target time => expired tokens
-        int expired = distance(tokenLife.begin(),tokenLife.upper_bound(time));
-        
-        // total tokens - expired tokens
-        return tokenLife.size() - expired;
-    }
+    int countUnexpiredTokens(int ct) 
+    {
+        drop(ct);
+        return uniques;     
+    }
 };
