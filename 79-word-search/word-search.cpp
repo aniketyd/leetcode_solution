c47f19ec9ab090a30f4f49f6ab1a033500c9a3b8
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-04-29 13:22:53 +0530

    Time: 227 ms (74.07%) | Memory: 10.8 MB (62.79%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,33 +1,76 @@
 class Solution {
 public:
+    struct Node{
+    int x,y;
+    set<pair<int,int>>st;
+    Node(int a,int b){
+        x=a;
+        y=b;
+    }
+};
+  bool bf(vector<vector<char>>&v,string &word,int a,int b){
+         int n=v.size();
+        int m=v[0].size();
+        int sz=word.size();
+        int adjx[4]={0,1,-1,0};
+        int adjy[4]={1,0,0,-1};
+        queue<Node>q;
+        Node f(a,b);
+        f.st.insert({a,b});
+        q.push(f);
+        while(!q.empty()){
+            Node top=q.front();
+            q.pop();
+            if(top.st.size()==sz)return true;
+            for(int i=0;i<4;i++){
+                int nx=adjx[i]+top.x;
+                int ny=adjy[i]+top.y;
+                if(nx>=0&&ny>=0&&nx<n&&ny<m&&(top.st.find({nx,ny})==top.st.end())&&(word[top.st.size()]==v[nx][ny])){
+                    Node temp=top;
+                    temp.x=nx;
+                    temp.y=ny;
+                    temp.st.insert({nx,ny});
+                    q.push(temp);
+                }
+            }
+        }
+        return false;
+  }
+    vector<vector<int>> vis;
+    int adjx[4] = {0, 1, -1, 0};
+    int adjy[4] = {1, 0, 0, -1};
+    int N, M, S;
+
+    bool dfs(vector<vector<char>>& board, string &word, int x, int y, int idx) {
+        if (idx >= S) return true;
+
+        vis[x][y] = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + adjx[i];
+            int ny = y + adjy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M &&
+                !vis[nx][ny] && board[nx][ny] == word[idx]) {
+                if (dfs(board, word, nx, ny, idx + 1)) return true;
+            }
+        }
+
+        vis[x][y] = 0; 
+        return false;
+    }
+
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
-            return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
+        int n = board.size();
+        int m = board[0].size();
+        N = n; M = m; S = word.size();
+
+        vis.resize(n, vector<int>(m, 0));  
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, i, j, 1)) return true;
                 }
             }
         }
