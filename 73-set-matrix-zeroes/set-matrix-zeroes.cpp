38d22d5c6d052b9f6f26e3d46e0b7da5016da381
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-05-21 08:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (78.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mt) {
-        unordered_map<int,int>mpx,mpy;
+       bool f=false,f2=false;
         for(int i=0;i<mt.size();i++){
+            
             for(int j=0;j<mt[0].size();j++){
+                if(i==0&&mt[i][j]==0)f=true;
+                if(j==0&&mt[i][j]==0)f2=true;
                 if(mt[i][j]==0){
-                    mpx[i]=1;
-                    mpy[j]=1;
+                   mt[0][j]=0;
+                   mt[i][0]=0;
                 }
             }
         }
-        for(auto &x:mpx){
-           for(int j=0;j<mt[0].size();j++)
-           mt[x.first][j]=0;
+        for(int i=1;i<mt.size();i++){
+            if(mt[i][0]==0){
+                for(int j=0;j<mt[0].size();j++)
+                mt[i][j]=0;
+            }
         }
-        for(auto &y:mpy){
-           for(int i=0;i<mt.size();i++)
-           mt[i][y.first]=0;
+        for(int i=1;i<mt[0].size();i++){
+            if(mt[0][i]==0){
+                for(int j=0;j<mt.size();j++){
+                    mt[j][i]=0;
+                }
+            }
         }
+      if(f) {  for(int i=0;i<mt[0].size();i++)
+                mt[0][i]=0;
+        }
+        if(f2){
+            for(int i=0;i<mt.size();i++)
+            mt[i][0]=0;
+        }
+
     }
 };
