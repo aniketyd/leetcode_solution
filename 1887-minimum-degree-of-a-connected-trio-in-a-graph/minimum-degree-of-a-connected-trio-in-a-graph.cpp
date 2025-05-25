dd4d3fc80668611bbc466f4bd525c2852bf524f2
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-05-25 18:41:28 +0530

    Time: 79 ms (85.37%) | Memory: 47.4 MB (60.98%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-   int ans=1e9;
-  
-    void dfs(vector<vector<int>>&adj,int node,vector<int>&visited){
-        visited[node]=1;
-       
-       unordered_set<int>st;
-        for(auto adjnd:adj[node]){
-            if(visited[adjnd])continue;
-            st.insert(adjnd);
+    int minTrioDegree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adjMatrix(n+1, vector<int>(n+1, 0));
+        vector<int> indegree(n+1, 0);
+        for (auto &e:edges) {
+            adjMatrix[min(e[0], e[1])][max(e[0], e[1])] = 1;
+            indegree[e[0]]++;
+            indegree[e[1]]++;
         }
-       ;
-        for(auto nd:st){
-            for(auto adjnd:adj[nd]){
-                if(st.find(adjnd)!=st.end()){
-                    int temp=adj[node].size()+adj[nd].size()+adj[adjnd].size();
-                    ans=min(ans,temp-6);
+        int ans = INT_MAX;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i+1; j <= n; j++) {
+                if (adjMatrix[i][j]) {
+                    for (int k = j+1; k <= n; k++) {
+                        if (adjMatrix[i][k] && adjMatrix[j][k]) {
+                            ans = min(ans, indegree[i] + indegree[j] + indegree[k]-6);
+                        }
+                    }
                 }
             }
         }
-        
+        return ans == INT_MAX ? -1 : ans;
 
-        for(auto adjnd:adj[node]){
-            if(visited[adjnd]==0)
-            dfs(adj,adjnd,visited);
-        }
-    }
-    int minTrioDegree(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>adj(n+1);
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int>visited(n+1);
-        for(int i=1;i<=n;i++){
-            if(visited[i]==0){
-                dfs(adj,i,visited);
-            }
-        }
-        if(ans>1e6)return -1;
-        return ans;
     }
 };
