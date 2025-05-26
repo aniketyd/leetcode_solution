8885f172bcb724df571d799a36c4e9fc6ab34272
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-05-26 09:37:32 +0530

    Time: 12 ms (96.73%) | Memory: 18.9 MB (94.91%) - LeetSync

--- 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int findShortestCycle(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-
-        int ans = INT_MAX;
-
-        for (int i = 0; i < n; ++i) {
-            vector<int> dist(n, -1);
-            vector<int> parent(n, -1);
-            queue<int> q;
-            q.push(i);
-            dist[i] = 0;
-
-            while (!q.empty()) {
-                int node = q.front(); q.pop();
-                for (int neighbor : adj[node]) {
-                    if (dist[neighbor] == -1) {
-                        dist[neighbor] = dist[node] + 1;
-                        parent[neighbor] = node;
-                        q.push(neighbor);
-                    } else if (parent[node] != neighbor) {
-                        // Found a cycle
-                        ans = min(ans, dist[node] + dist[neighbor] + 1);
-                    }
+    int ans = INT_MAX;
+    vector<vector<int>> g;
+    vector<int> depth;
+    void dfs(int v, int parent, int d) {
+        depth[v] = d;
+        for (int u : g[v]) {
+            if (u != parent) {
+                if (depth[u] > d + 1) dfs(u, v, d + 1);
+                else if (depth[u] < d) {
+                    ans = min(ans, d-depth[u]+1);
                 }
             }
         }
+    }
 
-        return (ans == INT_MAX ? -1 : ans);
+    int findShortestCycle(int n, vector<vector<int>>& edges) {
+        g.resize(n);
+        depth.resize(n, INT_MAX);
+        for (auto e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        for (int i=0; i<n; i++) {
+            if (depth[i] == INT_MAX) dfs(i, -1, 0);
+        }
+        return ans==INT_MAX?-1:ans;
     }
 };
