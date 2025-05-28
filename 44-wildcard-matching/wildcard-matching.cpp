ed9e2e508ec1fb8a8d1d1656c80456dccc9c9a8c
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-05-28 19:05:25 +0530

    Time: 34 ms (47.95%) | Memory: 29.8 MB (42.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-vector<vector<int>>dp;
+
  
     bool isMatch(string s, string p) {
         int n1=s.size();
         int n2=p.size();
-        dp.resize(n1+1,vector<int>(n2+1,0));
-        dp[0][0]=1;
+        vector<int>dp(n2+1,0);
+        dp[0]=1;
         for(int i=0;i<n2;i++){
             if(p[i]=='*')
-            dp[0][i+1]=1;
+            dp[i+1]=1;
             else
             break;
         }
         for(int i=1;i<=n1;i++){
+            vector<int>curDp(n2+1,0);
             for(int j=1;j<=n2;j++){
                 bool res=false;
                    if(p[j-1]=='?'||s[i-1]==p[j-1])
-                           res|=dp[i-1][j-1];
+                           res|=dp[j-1];
                    if(p[j-1]=='*'){
-                            res|=dp[i-1][j];
-                             res|=dp[i][j-1];
+                            res|=dp[j];
+                             res|=curDp[j-1];
                     }
-                    dp[i][j]=res;
+                    curDp[j]=res;
             }
+            dp=curDp;
         }
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
