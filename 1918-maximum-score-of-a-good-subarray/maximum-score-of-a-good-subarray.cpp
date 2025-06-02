b1965beff78209600ce9c14fcf4ecb78da286792
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-06-02 10:07:02 +0530

    Time: 4 ms (62.32%) | Memory: 93.4 MB (52.00%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
@@ -12,27 +12,21 @@ public:
         for(int i=k;i<n;i++){
             minEle=min(minEle,nums[i]);
             nums[i]=minEle;
-            auto greaterElements=lower_bound(nums.begin(),nums.begin()+k,minEle)-(nums.begin()+k);
-            greaterElements=abs(greaterElements);
-            int possAns=(greaterElements+(i-k+1))*minEle;
-            ans=max(ans,possAns);
         }
+        int j=k+1;
         for(int i=k;i>=0;i--){
-            minEle=nums[i];
-            int greaterElements = n-k-1;
-auto it = lower_bound(nums.begin() + k + 1, nums.end(), minEle, greater<int>());
-if (it != nums.end()) {
-   
-    greaterElements = it - (nums.begin() + k+1);
-}
-        
-            int possAns=(greaterElements+(k-i+1))*minEle;
-            ans=max(ans,possAns);
+             while(j<n&&nums[j]>=nums[i]){
+                 ans=max(ans,(j-i)*nums[j]);
+                j++;
+             }
+             ans=max(ans,(j-i)*nums[i]);
+             
         }
-        for(int i=0;i<n;i++){
-            cout<<nums[i]<<' ';
-        }
-       
+         while(j<n){
+                ans=max(ans,(j+1)*nums[j]);
+                j++;
+             }
+      
 return ans;
     }
 };
