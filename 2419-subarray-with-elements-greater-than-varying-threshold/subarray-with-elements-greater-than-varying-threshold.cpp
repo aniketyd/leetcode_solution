441fe78816dd5566ff52fea6f5630fa4baf20265
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-06-08 10:30:23 +0530

    Time: 1372 ms (5.03%) | Memory: 96.1 MB (88.05%) - LeetSync

---
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int validSubarraySize(vector<int>& nums, int threshold) {
+        int n=nums.size();
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty()&&nums[st.top()]>=nums[i]){
+                int indx=st.top();
+                st.pop();
+                int prevIndx=-1;
+                if(!st.empty())
+                prevIndx=st.top();
+                if(nums[indx]>(threshold/(i-prevIndx-1)))
+                return (i-prevIndx-1);
+            }
+            st.push(i);
+            cout<<i<<endl;
+        }
+         while(!st.empty()){
+            int i=n;
+                int indx=st.top();
+                st.pop();
+                int prevIndx=-1;
+                if(!st.empty())
+                prevIndx=st.top();
+                if(nums[indx]>(threshold/(i-prevIndx-1)))
+                return (i-prevIndx-1);
+            }
+            return -1;
+    }
+};
