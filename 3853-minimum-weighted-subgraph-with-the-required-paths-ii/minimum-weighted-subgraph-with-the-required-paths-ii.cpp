acd74a3e3d62999f0647a9d835223a83ad0947fd
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-06-19 00:34:08 +0530

    Time: 616 ms (39.35%) | Memory: 308.1 MB (44.41%) - LeetSync

--- 3853-minimum-weighted-subgraph-with-the-required-paths-ii/minimum-weighted-subgraph-with-the-required-paths-ii.cpp
+++ 3853-minimum-weighted-subgraph-with-the-required-paths-ii/minimum-weighted-subgraph-with-the-required-paths-ii.cpp
@@ -1,80 +1,69 @@
 class Solution {
 public:
-void dfs(int u, int par, int lvl, int wt, vector<int> &levels, vector<int> &weights, vector<vector<pair<int, int>>> &graph, vector<vector<int>> &LCA) {
-        levels[u] = lvl;
-        weights[u] = wt;
-        LCA[u][0] = par;
-
-        for(auto &[v, w]: graph[u]) {
-            if(v != par) {
-                dfs(v, u, lvl + 1, wt + w, levels, weights, graph, LCA);
+vector<int>pS,lvl;
+vector<vector<int>>dp;
+    void dfs(vector<vector<pair<int,int>>>&adj,int node,int parent,int prefixSum,int level){
+          pS[node]=prefixSum;
+          lvl[node]=level;
+          if(parent!=-1){
+          dp[node][0]=parent;
+          for(int i=1;i<21;i++){
+             dp[node][i]=dp[dp[node][i-1]][i-1];
+          }
+          }
+          for(auto &[adjNode,wt]:adj[node]){
+            if(adjNode!=parent){
+                prefixSum+=wt;
+                dfs(adj,adjNode,node,prefixSum,level+1);
+                prefixSum-=wt;
             }
-        }
+          }
     }
-
-    void init(int n, int maxN, vector<vector<int>> &LCA) {
-        for(int i = 1; i <= maxN; i++) {
-            for(int j = 0; j < n; j++) {
-                if(LCA[j][i - 1] != -1) {
-                    int par = LCA[j][i - 1];
-                    LCA[j][i] = LCA[par][i - 1];
-                }
-            }
+    int lca(int node1,int node2){
+        if(node1==node2)return node1;
+        if(lvl[node1]<lvl[node2])swap(node1,node2);
+        int levelDiff=lvl[node1]-lvl[node2];
+        int equalNode=node1;
+        for(int i=0;i<21;i++){
+            if((1<<i)&levelDiff)
+            equalNode=dp[equalNode][i];
         }
-    }
-
-    int findLCA(int a, int b, int maxN, vector<int> &levels, vector<vector<int>> &LCA) {
-        if(levels[a] > levels[b]) swap(a, b);
-
-        int d = levels[b] - levels[a];
-        while(d > 0) {
-            int jump = log2(d);
-            b = LCA[b][jump];
-            d -= (1LL << jump);
+        node1=equalNode;
+        if(node1==node2)return node1;
+        for(int i=20;i>=0;i--){
+            if(dp[node1][i]==dp[node2][i])continue;
+            node1=dp[node1][i];
+            node2=dp[node2][i];
         }
-
-        if(a == b) return a;
-
-        for(int i = maxN; i >= 0; i--) {
-            if(LCA[a][i] != -1 && LCA[a][i] != LCA[b][i]) {
-                a = LCA[a][i], b = LCA[b][i];
-            }
-        }
-
-        return LCA[a][0];
-    }
-
-    int getDistance(int u, int v, int maxN, vector<int> &levels, vector<int> &weights, vector<vector<int>> &LCA) {
-        int lca = findLCA(u, v, maxN, levels, LCA);
-        return weights[u] + weights[v] - 2 * weights[lca];
+        return dp[node1][0];
     }
     vector<int> minimumWeight(vector<vector<int>>& edges, vector<vector<int>>& queries) {
-                int n = edges.size() + 1, m = queries.size();
-
-        vector<vector<pair<int, int>>> graph(n);
-        for(auto &edge: edges) {
-            graph[edge[0]].push_back({edge[1], edge[2]});
-            graph[edge[1]].push_back({edge[0], edge[2]});
+        int n=edges.size()+1;
+        vector<vector<pair<int,int>>>adj(n+1);
+       
+        for(int i=0;i<n-1;i++){
+            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
         }
+        pS.resize(n);
+        lvl.resize(n);
+        dp.resize(n,vector<int>(22,0));
+        dfs(adj,0,-1,0,0);
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++){
+            int src1=queries[i][0];
+            int src2=queries[i][1];
+            int dest=queries[i][2];
+            int lca1=lca(src1,src2);
+            int lca2=lca(src2,dest);
+            int lca3=lca(src1,dest);
+           
+            long long sum=pS[src1]+pS[src2]-2*pS[lca1];
+            sum+=pS[src2]+pS[dest]-2*pS[lca2];
+            sum+=pS[src1]+pS[dest]-2*pS[lca3];
+            ans.push_back(sum/2);
 
-        int maxN = log2(n);
-        vector<int> levels(n);
-        vector<int> weights(n);
-        vector<vector<int>> LCA(n, vector<int> (maxN + 1, -1));
-        
-        dfs(0, -1, 0, 0, levels, weights, graph, LCA);
-        init(n, maxN, LCA);
-
-        vector<int> ans(m);
-        for(int i = 0; i < m; i++) {
-            int src1 = queries[i][0], src2 = queries[i][1], dest = queries[i][2];
-            
-            int total = (getDistance(src1, dest, maxN, levels, weights, LCA) + 
-                        getDistance(src2, dest, maxN, levels, weights, LCA) + 
-                        getDistance(src1, src2, maxN, levels, weights, LCA)) / 2;
-            ans[i] = total;
         }
-        
         return ans;
 
     }
