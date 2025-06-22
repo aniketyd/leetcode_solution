7b2d80a106321a9ba840f7319b8686afba1dd936
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-06-22 13:49:40 +0530

    Time: 80 ms (80.78%) | Memory: 117.4 MB (67.53%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
-        vector<int> minInterval(vector<vector<int>>& A, vector<int>& queries) {
-        priority_queue<vector<int>> pq;
-        unordered_map<int, int> res;
-        vector<int> Q = queries, res2;
-        sort(A.begin(), A.end());
-        sort(Q.begin(), Q.end());
-        int i = 0, n = A.size();
-        for (int q : Q) {
-            while (i < n && A[i][0] <= q) {
-                int l = A[i][0], r = A[i++][1];
-                pq.push({l - r - 1, r});
-            }
-            while (pq.size() && pq.top()[1] < q)
-                pq.pop();
-            res[q] = pq.size() ? -pq.top()[0] : -1;
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        int n = queries.size();
+        vector<pair<int, int>> sortedQueries;
+        for (int i = 0; i < n; ++i) {
+            sortedQueries.push_back({queries[i], i});
         }
-        for (int q : queries)
-            res2.push_back(res[q]);
-        return res2;
+
+        sort(sortedQueries.begin(), sortedQueries.end());
+        sort(intervals.begin(), intervals.end());
+
+        vector<int> result(n, -1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        int i = 0;
+
+        for (auto& [query, idx] : sortedQueries) {
+            // Add all intervals whose left <= query
+            while (i < intervals.size() && intervals[i][0] <= query) {
+                int l = intervals[i][0], r = intervals[i][1];
+                minHeap.push({r - l + 1, r});
+                ++i;
+            }
+
+            while (!minHeap.empty() && minHeap.top().second < query) {
+                minHeap.pop();
+            }
+
+            if (!minHeap.empty()) {
+                result[idx] = minHeap.top().first;
+            }
+        }
+
+        return result;
     }
 };
