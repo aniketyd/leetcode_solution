87f0c3b3bbd9d4808281f74413414a32972a1bda
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-07-08 13:35:57 +0530

    Time: 171 ms (41.45%) | Memory: 114.9 MB (79.54%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -2,18 +2,7 @@ class Solution {
 public:
     vector<int>temp;
     vector<vector<int>>dp;
-    int fun(vector<vector<int>>&events,int k,int n){
-            if(n<0||k==0)return 0;
-            if(dp[n][k]!=-1)return dp[n][k];
-            int take=events[n][2];
-            auto indx=lower_bound(temp.begin(),temp.end(),events[n][0])-temp.begin();
-            if(indx!=0)
-            take+=fun(events,k-1,indx-1);
-            int not_take=fun(events,k,n-1);
-            dp[n][k]=max(take,not_take);
-            return dp[n][k];
-
-    }
+  
     static bool cmp(vector<int>&v1,vector<int>&v2){
         return v1[1]<v2[1];
     }
@@ -23,8 +12,23 @@ public:
         for(int i=0;i<n;i++){
             temp.push_back(events[i][1]);
         }
-        dp.resize(n+1,vector<int>(k+1,-1));
-        return fun(events,k,n-1);
+        dp.resize(n+1,vector<int>(k+1,0));
+       
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k;j++){
+                int take=events[i][2];
+            auto indx=lower_bound(temp.begin(),temp.end(),events[i][0])-temp.begin();
+            if(indx!=0)
+            take+=dp[indx-1][j-1];
+            int not_take=0;
+            if(i!=0)
+         not_take=dp[i-1][j];
+            
+            dp[i][j]=max(take,not_take);
+            
+            }
+        }
+        return dp[n-1][k];
 
     }
 };
