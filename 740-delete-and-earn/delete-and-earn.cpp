3794fdfb07e2056a4a54f31fbb5ab18f39727fd6
aniketyd <125073425+aniketyd@users.noreply.github.com>
2025-08-22 22:22:08 +0530

    Time: 4 ms (51.64%) | Memory: 17.2 MB (48.88%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
+        
         map<int,int>mp;
         vector<int>v;
         for(int i=0;i<n;i++){
